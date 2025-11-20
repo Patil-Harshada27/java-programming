6ed5e3ed19e3f745d79505a241d095fd3599ea13
Patil-Harshada27 <harshadapatil2704@gmail.com>
2025-11-20 11:16:58 +0530

    Add files via upload

---
+++ program20.java
@@ -0,0 +1,20 @@
+
+//worst approch
+class program19
+{
+    public static void Display()
+    {
+        //sequance
+        System.out.println("Jay Ganesh....");
+        System.out.println("Jay Ganesh....");
+        System.out.println("Jay Ganesh....");
+        System.out.println("Jay Ganesh....");
+    }
+
+    public static void main(String A[])
+    {
+        Display();       
+        
+        
+    }
+}
